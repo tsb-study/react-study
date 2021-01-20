9b06c4e9a0e2368b663d186ff6e4219e3b044df1
qodbtn41 <qodbtn41@naver.com>
2021-01-21 01:33:51 +0900

    03-Component. 3.2까지

--- 03-Component/src/App.js
+++ 03-Component/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import MyComponent from "./MyComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   const name = "리액트";
+//   return <div className="react">{name}</div>;
+// }
+
+// class App extends Component {
+//   render() {
+//     const name = "react";
+//     return <div className="react">{name}</div>;
+//   }
+// }
+
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
