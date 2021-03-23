e7971a5ce43fe8beddb66e3694165b39218daae3
choisaelim <choisaelim@gmail.com>
2021-03-23 20:07:36 +0900

    링크, 라우터 path 배열 사용

--- 13-router/src/App.js
+++ 13-router/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 const App = () => {
 	return (
 		<div>
+			<ul>
+				<li>
+					{/* 링크를 통해 라우터 path와 연결 가능 */}
+					<Link to="/">홈</Link>
+				</li>
+				<li>
+					<Link to="/about">소개</Link>
+				</li>
+			</ul>
+			<hr />
 			<Route path="/" component={Home} exact={true} />
-			<Route path="/about" component={About} />
+			{/* 중괄호 내 배열로 여러 개의 path 사용 가능 */}
+			<Route path={['/about', '/info']} component={About} />
 		</div>
 	);
 };
