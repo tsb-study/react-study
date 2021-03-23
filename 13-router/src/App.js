706751c474615743aaeca82aa5699416dfbf0089
choisaelim <choisaelim@gmail.com>
2021-03-23 21:23:12 +0900

    서브 라우터 사용 - 메인 Profile에 링크 걸린 부분을 Profiles 파일로 이동

--- 13-router/src/App.js
+++ 13-router/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
 	return (
@@ -16,17 +16,14 @@ const App = () => {
 					<Link to="/about">소개</Link>
 				</li>
 				<li>
-					<Link to="/profile/velopert">velopert 프로필</Link>
-				</li>
-				<li>
-					<Link to="/profile/gildong">gildong 프로필</Link>
+					<Link to="/profiles">프로필</Link>
 				</li>
 			</ul>
 			<hr />
 			<Route path="/" component={Home} exact={true} />
 			{/* 중괄호 내 배열로 여러 개의 path 사용 가능 */}
 			<Route path={['/about', '/info']} component={About} />
-			<Route path="/profile/:username" component={Profile} />
+			<Route path="/profiles" component={Profiles} />
 		</div>
 	);
 };
