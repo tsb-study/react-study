acc4bc59e70836068357a9ddbc93604a96e58d3a
choisaelim <choisaelim@gmail.com>
2021-03-23 20:13:09 +0900

    파라미터 이용하기

--- 13-router/src/App.js
+++ 13-router/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 const App = () => {
 	return (
@@ -14,11 +15,18 @@ const App = () => {
 				<li>
 					<Link to="/about">소개</Link>
 				</li>
+				<li>
+					<Link to="/profile/velopert">velopert 프로필</Link>
+				</li>
+				<li>
+					<Link to="/profile/gildong">gildong 프로필</Link>
+				</li>
 			</ul>
 			<hr />
 			<Route path="/" component={Home} exact={true} />
 			{/* 중괄호 내 배열로 여러 개의 path 사용 가능 */}
 			<Route path={['/about', '/info']} component={About} />
+			<Route path="/profile/:username" component={Profile} />
 		</div>
 	);
 };
