023ee553383ed76b1641d9b06d7d6680fd73c753
choisaelim <choisaelim@gmail.com>
2021-03-24 22:36:24 +0900

    라우터 부가기능 History

--- 13-router/src/App.js
+++ 13-router/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import About from './About';
+import HistorySample from './HistorySample';
 import Home from './Home';
 import Profiles from './Profiles';
 
@@ -18,12 +19,16 @@ const App = () => {
 				<li>
 					<Link to="/profiles">프로필</Link>
 				</li>
+				<li>
+					<Link to="/history">History 예제</Link>
+				</li>
 			</ul>
 			<hr />
 			<Route path="/" component={Home} exact={true} />
 			{/* 중괄호 내 배열로 여러 개의 path 사용 가능 */}
 			<Route path={['/about', '/info']} component={About} />
 			<Route path="/profiles" component={Profiles} />
+			<Route path="/history" component={HistorySample} />
 		</div>
 	);
 };
