ffa62ac9c7734a4f558365aeccb8928a06ac533a
KimMiruJin <sccs604@naver.com>
2021-04-05 21:22:46 +0900

    used async await

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -6,10 +6,13 @@ import axios from 'axios';
 const App = () => {
   const [data, setData] = useState(null);
 
-  const onClick = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(response => {
-      setData(response.data);
-    });
+  const onClick = async () => {
+    try {
+      const response = await axios.get("https://newsapi.org/v2/top-headlines?country=kr&apikey=9390808960274625b9143c907ec9f436");
+      setData(response.data)
+    }catch(e){
+      console.log(e)
+    }
   };
     return(
       <div>
