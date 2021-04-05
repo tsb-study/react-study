bbaf52385f364b968518a2fdd26e0622fb6c860a
KimMiruJin <sccs604@naver.com>
2021-04-05 21:25:17 +0900

    News List App.js

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -2,27 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-
-  const onClick = async () => {
-    try {
-      const response = await axios.get("https://newsapi.org/v2/top-headlines?country=kr&apikey=9390808960274625b9143c907ec9f436");
-      setData(response.data)
-    }catch(e){
-      console.log(e)
-    }
-  };
-    return(
-      <div>
-        <div>
-          <button onClick = {onClick}>불러오기</button>
-        </div>
-        {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-      </div>
-    )
-  
+  return <NewsList/>
 };
 
 export default App;
