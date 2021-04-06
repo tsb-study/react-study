234ac31655447deec7f3e002e8b1a99f7246644d
KimMiruJin <sccs604@naver.com>
2021-04-06 23:11:09 +0900

    라우터 연동

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,20 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useCallback, useState } from 'react';
-import axios from 'axios';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
-
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
+ 
 const App = () => {
-  const [category, setCategory] = useState(null);
-  const onSelect = useCallback(category => setCategory(category), []);
-  
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect}/>
-      <NewsList category={category}/>
-    </>
-  )
+  return <Route path="/:category?" component={NewsPage} />;
 };
-
+ 
 export default App;
